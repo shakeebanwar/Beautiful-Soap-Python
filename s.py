b62d613a604c3ca7689bb2e5bfab4b608e084efa
shakeeb <shakeebanwar250@gmail.com>
2021-03-22 14:32:44 +0500

    done

--- s.py
+++ s.py
@@ -1,18 +1,20 @@
 import requests 
 from bs4 import BeautifulSoup 
-import csv 
+import csv
+from csv import DictWriter  
    
-URL = "https://turquoise.health/providers/abbott-northwestern-hospital/information"
+URL = "https://turquoise.health/providers/armstrong-county-memorial/information"
 r = requests.get(URL) 
 soup = BeautifulSoup(r.content, 'html5lib')
 c = 0
+quotes=[]
 quote = {} 
 for row in soup.findAll('div', attrs = {'class':'hospital-all-detail'}): 
     for j in row.findAll('ul'):
         for k in j.findAll('li'):
             c = c  + 1
-            print("count==> ",c)
-            print("span length ",k)
+            # print("count==> ",c)
+            # print("span length ",k)
             if len(k.span) == 1:
                 if c == 1:
                     quote['Name'] = k.span.text
@@ -37,7 +39,34 @@ for row in soup.findAll('div', attrs = {'class':'hospital-all-detail'}):
                 elif c == 9:
                     quote['BEDS'] = k.span.text
                 
-            
+            else:
+                if c == 6:
+                    if  k.span.a['href'] != "":
+                        quote['WEBSITE'] = k.span.a['href']
+
+                    else:
+                        quote['WEBSITE'] = "No link"
+
+                   
+                        
+                
+                else:
+                    quote['HEALTH SYSTEM AFFILIATION'] = k.span.a['href']
+        
+
+quotes.append(quote)
+print(quotes)
+columnsName = ['Name','ADDRESS','Phone','MEDICARE PROVIDER ID','NATIONAL PROVIDER ID (NPI)','WEBSITE','PROVIDER TYPE','OWNERSHIP','BEDS','HEALTH SYSTEM AFFILIATION']
+filename = 'hospital.csv'
+# with open(filename, 'w', newline='') as f: 
+#     w = csv.DictWriter(f,columnsName) 
+#     w.writeheader() 
+#     w.writerow(quotes[0])
 
 
-print(quote)
+
+with open(filename, 'a', newline='') as f_object:
+    dictwriter_object = DictWriter(f_object, fieldnames = columnsName)
+    dictwriter_object.writerow(quotes[0])
+        
+  
